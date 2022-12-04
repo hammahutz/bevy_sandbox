ace00d6eaa46499e29acfc575341cfb9497020b6
hammahutz <62766637+hammahutz@users.noreply.github.com>
2022-12-04 19:38:50 +0100

    First lesson commpleted

--- src/main.rs
+++ src/main.rs
@@ -17,5 +17,33 @@ fn main() {
             },
             ..default()
         }))
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_basic_scene)
         .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.67, 0.84, 0.93).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
 }
