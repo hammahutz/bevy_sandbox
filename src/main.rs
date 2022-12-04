428e90c86593d30130e17afd58055cc2f1cf614b
hammahutz <62766637+hammahutz@users.noreply.github.com>
2022-12-04 21:05:08 +0100

    lesson 2 till 4:32

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use std::f32::consts::PI;
+
+use bevy::{prelude::*, transform};
+use bevy_inspector_egui::WorldInspectorPlugin;
 
 pub const WIDTH: f32 = 1280.0;
 pub const HEIGHT: f32 = 720.0;
@@ -17,6 +20,7 @@ fn main() {
             },
             ..default()
         }))
+        .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_basic_scene)
         .run();
@@ -29,21 +33,73 @@ fn spawn_camera(mut commands: Commands) {
     });
 }
 
+//TODO Do i need this?
+// #[derive(Reflect, Component, Default)]
+// #[reflect(Component)]
+#[derive(Component)]
+pub struct Tower {
+    shoot_timer: Timer,
+}
+
 fn spawn_basic_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            ..default()
+        })
+        .insert(Name::new("Ground"));
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.67, 0.84, 0.93).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.67, 0.84, 0.93).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        })
+        .insert(Name::new("Tower"))
+        .insert(Tower {
+            shoot_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+        });
+
+    commands
+        .spawn(PointLightBundle {
+            point_light: PointLight {
+                intensity: 1500.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
+            ..default()
+        })
+        .insert(Name::new("Light"));
+}
+
+fn tower_shooting(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut towers: Query<&mut Tower>,
+    time: Res<Time>,
+) {
+    for mut tower in &mut towers {
+        tower.shoot_timer.tick(time.delta());
+
+        if tower.shoot_timer.just_finished() {
+            let spawn_transform =
+                Transform::from_xyz(0.0, 0.7, 0.6).with_rotation(Quat::from_rotation_y(-PI / 2.0));
+
+            commands.spawn(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
+                material: materials.add(Color::rgb(0.87, 0.44, 0.42).into()),
+                transform: spawn_transform,
+                ..default()
+            })
+            .insert(Name::new("Bullet"));
+        }
+    }
 }
